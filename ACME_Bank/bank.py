3be3e30756f39fdb0bf1ba2d86531e92e1f4713a
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-09-23 03:33:43 +0300

    Created generate_account_id function

--- ACME_Bank/bank.py
+++ ACME_Bank/bank.py
@@ -1,5 +1,8 @@
-#Main page for ACME
+import random
+
+# Main page for ACME bank system
 class mainBankPage:
+    # Function displaying main page welcome message
     def welcomeMessage():
         print()
         print("---------------------------------------------------")
@@ -8,8 +11,12 @@ class mainBankPage:
         print("  Please choose one of the following options:")
         print("    1.Login to existing account")
         print("    2.Create new checking account")
-        print("    2.Create new saving account")
-        print("    3.Exit")
+        print("    3.Create new saving account")
+        print("    4.Exit")
         print()
     welcomeMessage()
+
+# function for generating random and unique account_id
+def generate_account_id():
+    return random.randint (10000,99999)
 
