59f523db37be99ff074379646c7736954ee71eaf
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-09-23 03:19:24 +0300

    Created mainBankPage class and WelcomeMessage function

---
+++ ACME_Bank/bank.py
@@ -0,0 +1,15 @@
+#Main page for ACME
+class mainBankPage:
+    def welcomeMessage():
+        print()
+        print("---------------------------------------------------")
+        print("                Welcome to ACME Bank               ")
+        print("---------------------------------------------------")
+        print("  Please choose one of the following options:")
+        print("    1.Login to existing account")
+        print("    2.Create new checking account")
+        print("    2.Create new saving account")
+        print("    3.Exit")
+        print()
+    welcomeMessage()
+
