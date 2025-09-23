434ba6dc03d5b55ed1aba2cfeb32fa0be41c0f16
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-09-23 06:11:56 +0300

    Created SavingsAccount class and initialized constructor

--- ACME_Bank/bank.py
+++ ACME_Bank/bank.py
@@ -5,14 +5,14 @@ class mainBankPage:
     # Function displaying main page welcome message
     def welcomeMessage():
         print()
-        print("---------------------------------------------------")
+        print("-" * 50)
         print("                Welcome to ACME Bank               ")
-        print("---------------------------------------------------")
+        print("-" * 50)
         print("  Please choose one of the following options:")
-        print("    1.Login to existing account")
-        print("    2.Create new checking account")
-        print("    3.Create new saving account")
-        print("    4.Exit")
+        print("    1.Login to existing account.")
+        print("    2.Create new checking account.")
+        print("    3.Create new saving account.")
+        print("    4.Exit.")
         print()
     welcomeMessage()
 
@@ -29,16 +29,41 @@ class BankAccount:
     def deposit(self, amount):
         if amount >0:
             self.balance += amount
-            print(f"Deposited ${amount}. \n New balance: ${self.balance}")
+            print(f"Deposited ${amount}\nNew balance: ${self.balance}")
         else:
-            print("Deposit amount must be positive")
+            print("Deposit amount must be positive.")
     
     def withdraw(self,amount):
         if 0 <= amount <= self.balance:
             self.balance -= amount
-            print(f"Withdrew ${amount}. \n New balance: ${self.balance}")
+            print(f"Withdrew ${amount}\nNew balance: ${self.balance}")
         else:
             print ("Insufficient balance or invalid withdrawal amount.")
 
     def display_details(self):
-        print(f"Account ID: {self.account_id}. \n Account Holder: {self.account_holder}.\n Balance: {self.balance}")
+        print(f"Account ID: {self.account_id}, Account Holder: {self.account_holder}, Balance: {self.balance}")
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, account_holder, minimum_balance):
+        super.__init__(account_holder)
+        self.minimum_balance  = minimum_balance
+        
+    
+#bank_account1 = BankAccount("Michael")
+#bank_account1.deposit(100)
+#bank_account1.deposit(100)
+#bank_account1.withdraw(50)
+
+#print("-" * 50)
+
+#bank_account2 = BankAccount("Kalen")
+#bank_account2.deposit(500)
+#bank_account2.deposit(-30)
+#bank_account2.withdraw(780)
+
+#print("-" * 50)
+
+#bank_account1.display_details()
+#bank_account2.display_details()
+
